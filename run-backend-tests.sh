5d2048d33099b47ac7a52096bd1dad28131823c8
Benoît Hubert <benoithubert@gmail.com>
2023-04-24 15:15:17 +0200

    change params order

--- run-backend-tests.sh
+++ run-backend-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose -f docker-compose.yml -f docker-compose.test.backend.yml up mysql backend --exit-code-from backend
+docker-compose -f docker-compose.yml -f docker-compose.test.backend.yml up --exit-code-from backend mysql backend
