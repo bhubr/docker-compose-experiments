7eb6c28eb5c30bd16c24c981e31f0f7c50759991
Benoît Hubert <benoithubert@gmail.com>
2023-04-24 11:54:02 +0200

    Extract error message sent by backend from axios error

--- react-web-client/src/ErrorAlert.jsx
+++ react-web-client/src/ErrorAlert.jsx
@@ -1,9 +1,8 @@
 import PropTypes from "prop-types";
 
 export default function ErrorAlert({ error }) {
-  return (
-    <p style={{ border: "2px solid red", padding: "1em" }}>{error.message}</p>
-  );
+  const message = error.response ? error.response.data.error : error.message;
+  return <p style={{ border: "2px solid red", padding: "1em" }}>{message}</p>;
 }
 
 ErrorAlert.propTypes = {
